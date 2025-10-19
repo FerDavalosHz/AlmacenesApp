c62408c36d2ad8b8112d7c0b6b1a0d9ef38df1d0
fernado davalos <fernadodavalos2005@gmail.com>
2025-10-19 10:21:35 -0600

    Formulario Usuarios CRUD

---
+++ AlmacenesApp/FrmUsuarios.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlmacenesApp
+{
+    public partial class FrmUsuarios : Form
+    {
+        public FrmUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegresar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            GbModificarAgregar.Visible = false;
+        }
+
+        private void BtnNuevo_Click(object sender, EventArgs e)
+        {
+            GbModificarAgregar.Visible = true;
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            GbModificarAgregar.Visible = true;
+        }
+
+        private void FrmUsuarios_Load(object sender, EventArgs e)
+        {
+            if (MenuPrincipal.Escritura == 1) { BtnEditar.Enabled = true; BtnEliminar.Enabled = true; BtnNuevo.Enabled = true; }
+        }
+    }
+}
